7cd3a2bfd3111d0814c66859e675eba79313e082
Jesus Esquivel <jesqivela@gmail.com>
2018-01-03 00:54:39 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-// import App from './App';
 import Frame from './components/frame';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Frame />, document.getElementById('root'));
-registerServiceWorker();
+
